64f02a360c40927fd718a7e034e1708088f3ac8d
範雷雷 <95039519+leileifan166@users.noreply.github.com>
2021-12-28 22:20:47 +0900

    Add files via upload

---
+++ javascript20/myjs.js
@@ -0,0 +1,25 @@
+document.write(100 + "200");
+document.write('<br>');
+document.write(100 - "200");
+document.write('<br>');
+document.write(true + 100);
+document.write('<br>');
+document.write(true + "100");
+document.write('<br>');
+document.write(true + false);
+document.write('<br>');
+document.write("a" - 100);
+document.write('<br>');
+document.write("a\\b");
+document.write('<br>');
+var a = null;
+var b;
+document.write(a);
+document.write('<br>');
+document.write(typeof a);
+document.write('<br>');
+document.write(b);
+
+
+
+
